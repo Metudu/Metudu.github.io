2191d21bf90e3c27100f9f0ab7c81fea09d2c73c
Metudu <tarikucar@gmail.com>
2024-05-15 16:47:29 +0300

    Main website structure has initialized

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/header/Header'
+import AboutMe from './components/about-me/AboutMe'
+import Section from './components/section/Section'
+import project3 from './assets/vpc-peering.png' 
+import project2 from './assets/gateway-endpoint-s3.png' 
+import project1 from './assets/aws-fargate.png' 
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header></Header>
+      <AboutMe></AboutMe>
+      <Section link="https://github.com/Metudu/orgf" header="orgf" paragraph="Are you tired of arranging your desktop or directories that you don't even know what it contains? orgf is here to help you. orgf is a open source CLI tool developed with Go that organizes directories. It can arrange files within a newly created sub-folder. It's easy to use and and learn."></Section>
+      <Section link="https://github.com/Metudu/dictigo" header="dictigo" paragraph="dictigo is a CLI tool that uses the DeepL Free API to translate texts. It can take multiple texts as arguments and translate them to the desired language."></Section>
+      <Section link="https://github.com/Metudu/Deploy-Docker-Containers-With-AWS-Fargate" image={project1} image_position="left" header="Docker images deployed to AWS Fargate" paragraph="I have deployed an NGINX container to AWS Fargate using Terraform as the IaC tool. Take a look at the GitHub repo if you're interested."></Section>
+      <Section link="https://github.com/Metudu/Gateway-Endpoint-For-S3" image={project2} image_position="right" header="Gateway Endpoint For S3" paragraph="I have deployed an AWS Gateway Endpoint for an AWS S3 Bucket using Terraform. This allows the S3 bucket to be used without accessing the public internet. Take a look at the repo if you're interested."></Section>
+      <Section link="https://github.com/Metudu/VPC-Peering-with-Terraform" image={project3} image_position="left" header="AWS VPC Peering" paragraph="I have created a peering connection between two AWS VPCs with Terraform. Take a look at the repo if you're interested."></Section>
     </>
   )
 }
