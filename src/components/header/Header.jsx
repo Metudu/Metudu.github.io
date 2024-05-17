cc2252e9434b52eb2b4680bd7f8b62de4e0b20f4
Metudu <tarikucar@gmail.com>
2024-05-17 21:08:13 +0300

    header responsive design fix

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,22 @@
+import { useState } from 'react';
 import styles from './Header.module.css' 
+import { useRef } from 'react';
+import { useEffect } from 'react';
 
 export default function Header() {
+  const [height, setHeight] = useState(0);
+  const ref = useRef(null);
+
+  useEffect(() => {
+    setHeight(ref.current.clientHeight)
+  });
+
+
   return(
     <>
       <div className={styles.container}>
-        <div className={styles.header}>
+        <div className={styles.header} ref={ref} style={{top: `calc(50% - ${height / 2}px)`}}>
+          {height ? null : null}
           <p id={styles.welcome}>Welcome</p>
           <p id={styles.name}>I am Tarık</p>
           <p id={styles.location}>from Istanbul, Turkey</p>
