f553982dcaf9a0c3a111b810e1aa3f2a4fc3a686
Metudu <tarikucar@gmail.com>
2024-05-17 21:49:19 +0300

    new tab for a tag

--- src/components/section/Section.jsx
+++ src/components/section/Section.jsx
@@ -6,7 +6,7 @@ export default function Section(props) {
       <div className={styles.container}>
         {props.image != null ? <img src={props.image} alt="image" /> : null}
         <div className={styles.text}>
-          <p className={styles.header}><a href={props.link} target='_blank' rel='noreferrer noopener'>{props.header}</a></p>
+          <p className={styles.header}><a href={props.link} rel='noreferrer noopener' target='_blank'>{props.header}</a></p>
           <hr />
           <p className={styles.paragraph}>{props.paragraph}</p>
         </div>
